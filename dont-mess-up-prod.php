93a575d901babdc9415f0cf886d8cb006d0787c7
Dave Kellam <kellam@gmail.com>
2025-11-21 14:51:17 -0500

    Fix version bump 😅

--- dont-mess-up-prod.php
+++ dont-mess-up-prod.php
@@ -19,9 +19,7 @@
 
 namespace DontMessUpProd;
 
-use DontMessUpProd\Environment_Indicator;
-
-define( 'DONT_MESS_UP_PROD_VERSION', '0.6.0' );
+define( 'DONT_MESS_UP_PROD_VERSION', '0.7.0' );
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-environment-indicator.php';
 Environment_Indicator::get_instance();
