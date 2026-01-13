1fb36837875b5e2ad4855ce8b0da4caad8b555a0
Dave Kellam <kellam@gmail.com>
2026-01-12 19:14:24 -0500

    Remove some questionable javascript
    May have needed this with some previous overly complicated tests, but this would silently eat a lot of things 😬
    
    #4

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,9 +1,2 @@
 // Import commands
 import './commands'
-
-// Prevent Cypress from failing on uncaught exceptions
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // Returning false here prevents Cypress from failing the test
-  // WordPress admin sometimes has benign JS errors
-  return false
-})
